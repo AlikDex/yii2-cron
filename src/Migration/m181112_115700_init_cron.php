3dbeeea41f95782429f4970a48c1c15efb84454c
alikdexor <alikdexor@gmail.com>
2019-01-14 15:44:16 +0300

    Change charset and uppercase unsigned

--- src/Migration/m181112_115700_init_cron.php
+++ src/Migration/m181112_115700_init_cron.php
@@ -14,23 +14,20 @@ class m181112_115700_init_cron extends Migration
     {
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
 
         $this->createTable('cron_tasks', [
-            'task_id' => 'smallint(5) unsigned NOT NULL',
+            'task_id' => 'smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
             'expression' => 'varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT \'\'',
-            'command' => 'text CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL',
-            'priority' => 'smallint(5) unsigned NOT NULL DEFAULT 1000',
+            'command' => 'text CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL',
+            'priority' => 'smallint(5) UNSIGNED NOT NULL DEFAULT 1000',
             'last_execution' => 'timestamp NULL DEFAULT NULL',
-            'duration' => 'double unsigned DEFAULT 0',
+            'duration' => 'double UNSIGNED DEFAULT 0',
             'status' => 'enum(\'\',\'planned\',\'running\',\'completed\',\'failed\',\'aborted\') COLLATE latin1_general_ci NOT NULL DEFAULT \'\'',
-            'enabled' => 'tinyint(3) unsigned NOT NULL DEFAULT 1',
+            'enabled' => 'tinyint(3) UNSIGNED NOT NULL DEFAULT 1',
             'created_at' => 'timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP',
         ], $tableOptions);
-
-        $this->addPrimaryKey('task_id', 'cron_tasks', 'task_id');
-        $this->execute("ALTER TABLE `cron_tasks` MODIFY `task_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT");
     }
 
     /**
